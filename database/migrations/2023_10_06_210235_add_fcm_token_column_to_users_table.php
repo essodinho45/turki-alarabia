8cd6401fa490f8372f0ce7e2f74db9da01d13903
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2023-10-07 00:54:07 +0300

    x

--- database/migrations/2023_10_06_210235_add_fcm_token_column_to_users_table.php
+++ database/migrations/2023_10_06_210235_add_fcm_token_column_to_users_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('fcm_token')->nullable();
+            $table->text('fcm_token')->nullable()->default(NULL);
         });
     }
 
