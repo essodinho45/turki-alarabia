4fb6022fb630988e029e06a2c605886bb3f0ff53
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2023-10-14 23:33:11 +0300

    route fix

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Transaction;
+use App\Http\Controllers\FirebasePushController;
 
 /*
 |--------------------------------------------------------------------------
