e7933cecbb21f1e7698ee92329bacb5090e39832
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2023-11-02 02:32:41 +0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,3 @@
-var window = require("sdk/window/utils").getMostRecentBrowserWindow();
-AudioObj = new window.Audio;
-
 self.addEventListener('push', function (e) {
     if (!(self.Notification && self.Notification.permission === 'granted')) {
         //notifications aren't supported or permission not granted!
@@ -10,7 +7,6 @@ self.addEventListener('push', function (e) {
     if (e.data) {
         var msg = e.data.json();
         console.log(msg);
-        AudioObj.play('sound.mp3');
         e.waitUntil(self.registration.showNotification(msg.title, {
             body: msg.body,
             icon: msg.icon,
