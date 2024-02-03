8973daa13eef9b770d3fed72626dab5cbec55857
essodinho45@gmail.com <ALKHAZENSOFT\isam>
2024-02-03 14:22:03 +0300

    disable print order

--- resources/views/livewire/create-buying-order.blade.php
+++ resources/views/livewire/create-buying-order.blade.php
@@ -89,10 +89,10 @@
                 {{ __('Close') }}
             </x-secondary-button>
 
-            <a href="{{ route('transactions.printOrder', ['transaction' => $id ?? 0]) }}" target="_blank"
+            {{-- <a href="{{ route('transactions.printOrder', ['transaction' => $id ?? 0]) }}" target="_blank"
                 class="inline-flex items-center px-4 py-2 bg-blue-700 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-800 focus:bg-blue-800 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition ease-in-out duration-150 rtl:mr-3 ltr:ml-3">
                 {{ __('Print') }}
-            </a>
+            </a> --}}
         </x-slot>
     </x-dialog-modal>
 </div>
